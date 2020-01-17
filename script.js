f98d1372ef41cfaf727124bc433f70a7f0824125
Vibhor Sharma <vibhor5000@gmail.com>
2020-01-17 19:24:06 +0530

    Update script.js

--- script.js
+++ script.js
@@ -14,8 +14,8 @@ function onFormSubmit() {
 function readFormData() {
     var formData = {};
     formData["fullName"] = document.getElementById("fullName").value;
-    formData["empCode"] = document.getElementById("empCode").value;
-    formData["salary"] = document.getElementById("salary").value;
+    formData["DOB"] = document.getElementById("DOB").value;
+    formData["Phone_No"] = document.getElementById("Phone_No").value;
     formData["city"] = document.getElementById("city").value;
     return formData;
 }
@@ -26,9 +26,9 @@ function insertNewRecord(data) {
     cell1 = newRow.insertCell(0);
     cell1.innerHTML = data.fullName;
     cell2 = newRow.insertCell(1);
-    cell2.innerHTML = data.empCode;
+    cell2.innerHTML = data.DOB;
     cell3 = newRow.insertCell(2);
-    cell3.innerHTML = data.salary;
+    cell3.innerHTML = data.Phone_No;
     cell4 = newRow.insertCell(3);
     cell4.innerHTML = data.city;
     cell4 = newRow.insertCell(4);
@@ -38,8 +38,8 @@ function insertNewRecord(data) {
 
 function resetForm() {
     document.getElementById("fullName").value = "";
-    document.getElementById("empCode").value = "";
-    document.getElementById("salary").value = "";
+    document.getElementById("DOB").value = "";
+    document.getElementById("Phone_No").value = "";
     document.getElementById("city").value = "";
     selectedRow = null;
 }
@@ -47,14 +47,14 @@ function resetForm() {
 function onEdit(td) {
     selectedRow = td.parentElement.parentElement;
     document.getElementById("fullName").value = selectedRow.cells[0].innerHTML;
-    document.getElementById("empCode").value = selectedRow.cells[1].innerHTML;
-    document.getElementById("salary").value = selectedRow.cells[2].innerHTML;
+    document.getElementById("DOB").value = selectedRow.cells[1].innerHTML;
+    document.getElementById("Phone_No").value = selectedRow.cells[2].innerHTML;
     document.getElementById("city").value = selectedRow.cells[3].innerHTML;
 }
 function updateRecord(formData) {
     selectedRow.cells[0].innerHTML = formData.fullName;
-    selectedRow.cells[1].innerHTML = formData.empCode;
-    selectedRow.cells[2].innerHTML = formData.salary;
+    selectedRow.cells[1].innerHTML = formData.DOB;
+    selectedRow.cells[2].innerHTML = formData.Phone_No;
     selectedRow.cells[3].innerHTML = formData.city;
 }
 
